d1ad42d56ddca7688990b1d41455bb82be173366
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-13 08:51:06 -0600

    add constuctor that is invoked when the programs compiler runs

--- Dog.java
+++ Dog.java
@@ -1,8 +1,15 @@
 public class Dog {
+    String breed;
+    String hasOwner;
+    int age;
 
+    public Dog() {
+        System.out.println("Constructor invoked!");
+        
+    }
 
 
     public static void main(String[] args) {
-        
+
     }
 }
