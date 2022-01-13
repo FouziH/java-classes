ccf6030153fb278280deea4f1e28220d2b47a4ce
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-13 09:05:29 -0600

    Created a instance of Dog contructor

--- Dog.java
+++ Dog.java
@@ -15,6 +15,10 @@ public class Dog {
     public static void main(String[] args) {
         System.out.println("Main Method Started");
         Dog fido = new Dog("poodle", false, 4);
+        Dog nunzio = new Dog("shiba inu", true, 12);
+        boolean isFidoOlder = fido.age > nunzio.age;
+        int totalDogYears = fido.age  + nunzio.age;
+        
 
     }
 }
